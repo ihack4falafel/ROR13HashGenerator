5b4fdff4040f3444b2ceca5e9158608902c212a4
Hashim Jawad <hashem.jawad@yahoo.com>
2019-05-26 12:39:13 -0400

    Push files

---
+++ ROR13HashGenerator/ROR13HashGenerator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// reference: https://www.tophertimzen.com/blog/shellcodeTechniquesCPP/
+namespace ROR13HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Usage: ROR13HashGenerator <function name>");
+                Console.ForegroundColor = ConsoleColor.White;
+                System.Environment.Exit(1);
+            }
+            string FunctionName = args[0];
+            int functionPreHash = (int)ROR13Hash(FunctionName, 13);
+            int functionHash = (int)(functionPreHash & 0xFFFFFFFF);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Function Name = {0}", FunctionName);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("ROR13 Hash    = 0x{0:X}", functionHash);
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Environment.Exit(1);
+        }
+
+        public static uint ROR13Hash(string FunctionName, int bits)
+        {
+            uint functionHash = 0;
+            foreach (char c in FunctionName)
+            {
+                uint i = (uint)c;
+                functionHash = ROR13(functionHash, 13);
+                functionHash = (functionHash + i);
+            }
+            return functionHash;
+        }
+
+        public static uint ROR13(uint FucntionHash, int bits)
+        {
+            return ((FucntionHash >> bits | FucntionHash << (32 - bits)) & 0xFFFFFFFF);
+        }
+    }
+}
